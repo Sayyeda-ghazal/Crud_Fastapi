be8b0afdc0958de8e609081b55fe2c874e99372d
Syeda Ghazal <99088845+Sayyeda-ghazal@users.noreply.github.com>
2025-01-14 22:34:43 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel , Field, validator
+from pydantic import BaseModel , Field, validator, EmailStr
 
 class Book(BaseModel):
     task : str = Field(description="Name of product", min_length=3, max_length=20)
@@ -7,6 +7,20 @@ class Book(BaseModel):
 
     @validator('task')
     def check_for_numbers(cls, value):
-        if any(char.isdigit() for char in value):  # Check if any character is a number
+        if any(char.isdigit() for char in value):  
             raise ValueError("Task cannot contain any numbers")
         return value
+    
+class login(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class createuserrequest(BaseModel):
+    username:str
+    email: EmailStr
+    password:str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
