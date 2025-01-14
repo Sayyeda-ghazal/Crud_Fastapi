621ec81190872a6a6c4e633f02c393f2c6d13406
Syeda Ghazal <99088845+Sayyeda-ghazal@users.noreply.github.com>
2025-01-14 22:34:19 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -9,6 +9,8 @@ from models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
+import models
+from schemas import createuserrequest, login
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -17,14 +19,7 @@ ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 outh2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
-
-class createuserrequest(BaseModel):
-    username:str
-    password:str
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
+ACCESS_TOKEN_EXPIRE_MINUTES=30
 
 def get_db():
     db = SessionLocal()
@@ -35,59 +30,34 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-async def get_current_user(token: str = Depends(outh2_bearer), db: Session = Depends(get_db)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("id")
-        if user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-            )
-        user = db.query(Users).filter(Users.id == user_id).first()
-        if user is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="User not found",
-            )
-        return user
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-        )
+def create_access_token(data: dict):
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode = data.copy()
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-@router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    user = authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='incorrect username')
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
-    return{'access_token': token, 'token_type': 'bearer'}
+@router.post("/signup", status_code=status.HTTP_201_CREATED)
+def create_user( user: createuserrequest, session: Session = Depends(get_db)):
+    new_user = models.Users()
+    existing_user = session.query(models.Users).filter(models.Users.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail=f"User with name {user.username} already exists")
+    new_user.username = user.username
+    new_user.password = bcrypt_context.hash(user.password)
+    new_user.email = user.email
+    existing_user = session.query(models.Users).filter(models.Users.username == user.username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail=f"User with name {user.username} already exists")
+    session.add(new_user)
+    session.commit()
+    return new_user
 
-def authenticate_user(username: str, password: str, db):
-    user = db.query(Users).filter(Users.username == username).first()
-    if not user:
-        return False
-    if not bcrypt_context.verify(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
-    encode = {'sub':username, 'id': user_id}
-    expires = datetime.utcnow() + expires_delta
-    encode.update({'exp':expires})
-    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
-
-async def get_current_user(token: Annotated[str, Depends(outh2_bearer)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get('sub')
-        user_id: str = payload.get('id')
-        if username is None or user_id is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate user.")
-        return {'username': username, 'id': user_id}
-    except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= 'Could not validate user')
-
+@router.get("/login")
+def login_user(user: login, session: Session = Depends(get_db)):
+    db_user = session.query(models.Users).filter(models.Users.username == user.username).first()
+    if not db_user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "User not found")
+    if not bcrypt_context.verify(user.password,db_user.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail= "Incorrect password")
+    access_token = create_access_token(data={"sub": db_user.username})
+    return {"message": "Login successful", "username": user.username, "access_token": access_token, "token_type": "bearer"}
