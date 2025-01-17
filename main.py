7a92128bd2e643663e96a0f6e64dde985a3129ea
Syeda Ghazal <99088845+Sayyeda-ghazal@users.noreply.github.com>
2025-01-17 16:02:56 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from fastapi import FastAPI, Depends,  Request
-from database import Base, engine
-from sqlalchemy.orm import Session
-import auth
-import random
-import string
-from auth import get_db , login_user
-from typing import Annotated
+import os
+from dotenv import load_dotenv, dotenv_values
 
-app = FastAPI()
-app.include_router(auth.router)  
-
-Base.metadata.create_all(engine)
+load_dotenv()
 
 
-@app.middleware("http")
-async def request_id_logging(request: Request, call_next):
-    response = await call_next(request)
-    random_letters = ''.join(random.choice(string.ascii_letters) for _ in range(10))
-    print(f'Log {random_letters}')
-    response.headers["X-Request-ID"] = random_letters
-    return response
+config = dotenv_values(".env")
+print(config)
 
+
+config ={ 
+    **dotenv_values(".env.shared"),
+    **dotenv_values(".env.secret")
+
+}
+# print(os.getenv("MY_SECRET_KEY"))
+
+# print(os.getenv("COMBINED"))
+
+# print(os.getenv("MAIL"))
