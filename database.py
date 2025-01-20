44ee2b1ed12c538e5904261acdf6360e348ad65e
Syeda Ghazal <99088845+Sayyeda-ghazal@users.noreply.github.com>
2025-01-20 16:11:20 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = 'postgresql://postgres:ghazaldb1@localhost:5432/Book'
+load_dotenv(dotenv_path=r"C:\Users\s\Desktop\apivenv\.env.secret")
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+DEBUG_MODE = os.getenv("DEBUG", "False") == "True"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine, expire_on_commit= False)
